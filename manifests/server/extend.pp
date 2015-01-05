663a8b09cfde194317fcb320d4cb75d380674a9b
Kenny Rasschaert <kenny@kennyrasschaert.be>
2015-01-05 15:11:34 +0100

    Add $ to regex for finding snmp cache duration

--- manifests/server/extend.pp
+++ manifests/server/extend.pp
@@ -13,7 +13,7 @@ define snmp::server::extend (
   }
   exec { "$extension_name snmp cache duration":
     command => "snmpset -v2c -c private localhost \'NET-SNMP-EXTEND-MIB::nsExtendCacheTime.\"$extension_name\"\' i $cache_duration",
-    unless  => "snmpget -v2c -c public localhost \'NET-SNMP-EXTEND-MIB::nsExtendCacheTime.\"$extension_name\"\' | grep -q \" $cache_duration\"",
+    unless  => "snmpget -v2c -c public localhost \'NET-SNMP-EXTEND-MIB::nsExtendCacheTime.\"$extension_name\"\' | grep -q -e \" $cache_duration\$\"",
     path    => '/bin',
     require =>  [
                   Class['snmp::client'],
